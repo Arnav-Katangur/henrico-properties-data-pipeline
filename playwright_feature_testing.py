955799272e79d1b4676efa441bdf0293366b7dd4
Arnav Katangur <katangur.arnav@gmail.com>
2026-04-28 21:00:26 -0400

    Read network data in playwright

--- playwright_feature_testing.py
+++ playwright_feature_testing.py
@@ -1,8 +1,23 @@
 from playwright.sync_api import sync_playwright
 
+def log_request(request):
+    print("\nREQUEST")
+    print("method:", request.method)
+    print("url:", request.url)
+
+
+def log_response(response):
+    print("\nRESPONSE")
+    print("status:", response.status)
+    print("url:", response.url)
+
 with sync_playwright() as p:
-    browser = p.chromium.launch(headless=True)
+    browser = p.chromium.launch(headless=False)
     page = browser.new_page()
+
+    page.on("request", log_request)
+    page.on("response", log_response)
+
     page.goto("https://realestate.henrico.gov/")
-    print(page.content())
+
     browser.close()
